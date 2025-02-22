bfe0c8c08f7db00e6261d6a619f9a76b46470cd2
NoCoDeJo <46725th@gmail.com>
2025-02-22 00:13:01 -0500

    chore: add next.config.js with build settings

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['raw.githubusercontent.com', 'assets.coingecko.com'],
+  },
+}
